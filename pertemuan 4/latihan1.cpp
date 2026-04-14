e05ff009bcced52d149b739cf6d6957b8daa433f
Daniel Sinurat <133616637+Dan1eLsnrt@users.noreply.github.com>
2026-04-14 12:23:58 +0700

    latihan1.cpp

--- pertemuan 4/latihan1.cpp
+++ pertemuan 4/latihan1.cpp
@@ -5,19 +5,16 @@ int main() {
     string kalimat;
     int i;
 
-    system("cls");
+    system("CLS");
 
-    cout << "Masukkan Kalimat : ";
+    cout << "Masukkan kalimat : ";
     getline(cin, kalimat);
 
     for (i = 0; i < kalimat.length(); i++){
-        if(kalimat[i]=='a' || kalimat[i]=='e' || kalimat[i]=='i' || 
-           kalimat[i]=='o' || kalimat[i]=='u'){
-            kalimat[i] = toupper(kalimat[i]);
-        }
+        kalimat[i] = toupper(kalimat[i]);
     }
 
-    cout << "Kalimat dengan vokal kapital : " << kalimat << endl;
+    cout << "Kalimat dalam huruf kapital : " << kalimat << endl;
 
     return 0;
 }
